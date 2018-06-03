663e8c986d35db1c40c09eead627487031abadeb
ClementTr <clement.tailleur@gmail.com>
2018-06-03 11:42:27 +0200

    Add all # to main views.py and urls.py and delete dept csv

--- DjangoWorldCup/worldcup/urls.py
+++ DjangoWorldCup/worldcup/urls.py
@@ -4,7 +4,33 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('main/', views.main, name='main'),
-    path('table/', views.table, name='table'),
     path('datacountries/',views.data_countries, name='main'),
-    path('dataplayers/',views.data_players, name='main')
+    path('dataplayers/',views.data_players, name='main'),
+
+    # path('final/', views.final, name='final'),
+    # path('third/', views.third, name='third'),
+    # path('semi_final_2/', views.semi_final_2, name='semi_final_2'),
+    # path('semi_final_1/', views.semi_final_1, name='semi_final_1'),
+    # path('quarter_final_2/', views.quarter_final_2, name='quarter_final_2'),
+    # path('quarter_final_1/', views.quarter_final_1, name='quarter_final_1'),
+    # path('round_16_4/', views.round_16_4, name='round_16_4'),
+    # path('round_16_3/', views.round_16_3, name='round_16_3'),
+    # path('round_16_2/', views.round_16_2, name='round_16_2'),
+    # path('round_16_1/', views.round_16_1, name='round_16_1'),
+    # path('engbel/', views.engbel, name='engbel'),
+    # path('serbra/', views.serbra, name='serbra'),
+    # path('fraden/', views.fraden, name='fraden'),
+    # path('spamar/', views.spamar, name='spamar'),
+    # path('polcol/', views.polcol, name='polcol'),
+    # path('gerswe/', views.gerswe, name='gerswe'),
+    # path('bracri/', views.bracri, name='bracri'),
+    # path('fraper/', views.fraper, name='fraper'),
+    # path('irnspa/', views.irnspa, name='irnspa'),
+    # path('rusegy/', views.rusegy, name='rusegy'),
+    # path('tuneng/', views.tuneng, name='tuneng'),
+    # path('brasui/', views.brasui, name='brasui'),
+    # path('fraaus/', views.fraaus, name='fraaus'),
+    # path('porspa/', views.porspa, name='porspa'),
+    # path('rusara/', views.rusara, name='rusara')
+
 ]
--- DjangoWorldCup/worldcup/views.py
+++ DjangoWorldCup/worldcup/views.py
@@ -11,15 +11,6 @@ def home(request):
         return redirect("main/")
     return render(request, 'worldcup/home.html')
 
-
-def main(request):
-    hashtag = "#FRAITA"
-    context = {'hashtag': hashtag}
-    return render(request, 'worldcup/main.html', context)
-
-def table(request):
-    return render(request, 'worldcup/table.html')
-
 def data_countries(request):
 	data = countriesCalculations("#FRAITA")
 	return JsonResponse(data,safe=False)
@@ -27,3 +18,135 @@ def data_countries(request):
 def data_players(request):
 	data = playersCalculations("#FRAITA")
 	return JsonResponse(data,safe=False)
+
+def main(request):
+    hashtag = "#FRAITA"
+    context = {'hashtag': hashtag}
+    return render(request, 'worldcup/main.html', context)
+
+
+
+# def final(request):
+#     hashtag = "#"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/main.html', context)
+#
+# def third(request):
+#     hashtag = "#"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/main.html', context)
+#
+# def semi_final_2(request):
+#     hashtag = "#"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/main.html', context)
+#
+# def semi_final_1(request):
+#     hashtag = "#"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/main.html', context)
+#
+# def quarter_final_2(request):
+#     hashtag = "#"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/main.html', context)
+#
+# def quarter_final_1(request):
+#     hashtag = "#"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/main.html', context)
+#
+# def round_16_4(request):
+#     hashtag = "#"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/main.html', context)
+#
+# def round_16_3(request):
+#     hashtag = "#"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/main.html', context)
+#
+# def round_16_2(request):
+#     hashtag = "#"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/main.html', context)
+#
+# def round_16_1(request):
+#     hashtag = "#"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/main.html', context)
+#
+# def engbel(request):
+#     hashtag = "#ENGBEL"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/engbel.html', context)
+#
+# def serbra(request):
+#     hashtag = "#SERBRA"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/serbra.html', context)
+#
+# def fraden(request):
+#     hashtag = "#FRADEN"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/fraden.html', context)
+#
+# def spamar(request):
+#     hashtag = "#SPAMAR"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/spamar.html', context)
+#
+# def polcol(request):
+#     hashtag = "#POLCOL"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/polcol.html', context)
+#
+# def gerswe(request):
+#     hashtag = "#GERSWE"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/gerswe.html', context)
+#
+# def bracri(request):
+#     hashtag = "#BRACRI"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/bracri.html', context)
+#
+# def fraper(request):
+#     hashtag = "#FRAPER"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/fraper.html', context)
+#
+# def irnspa(request):
+#     hashtag = "#IRNSPA"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/irnspa.html', context)
+#
+# def rusegy(request):
+#     hashtag = "#RUSEGY"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/rusegy.html', context)
+#
+# def tuneng(request):
+#     hashtag = "#TUNENG"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/tuneng.html', context)
+#
+# def brasui(request):
+#     hashtag = "#BRASUI"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/brasui.html', context)
+#
+# def fraaus(request):
+#     hashtag = "#FRAAUS"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/fraaus.html', context)
+#
+# def porspa(request):
+#     hashtag = "#PORSPA"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/porspa.html', context)
+#
+# def rusara(request):
+#     hashtag = "#RUSARA"
+#     context = {'hashtag': hashtag}
+#     return render(request, 'worldcup/rusara.html', context)
