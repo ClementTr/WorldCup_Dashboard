be87ace510171cb8836a3e4fc0f1401366beefad
Maxime Poulain <poulain@et.esiea.fr>
2018-06-14 23:33:46 +0200

    add timeseries percentage

--- DjangoWorldCup/worldcup/tools.py
+++ DjangoWorldCup/worldcup/tools.py
@@ -86,7 +86,7 @@ def positivity_negativity(hashtag_name):
     data['Negative'] = np.ceil((data['Negative'].astype(int)/data['cumul'])*100)
     data['Neutral'] = np.ceil((data['Neutral'].astype(int)/data['cumul'])*100)
     data['Positive'] = np.ceil((data['Positive'].astype(int)/data['cumul'])*100)
-    return data[["Time","Positive","Negatif"]].drop_duplicates('Time').to_json(orient="records")
+    return data[["Time","Positive","Negative"]].drop_duplicates('Time').to_json(orient="records")
     # return data.drop_duplicates('Time').to_json(orient="records")
 
 def get_Emojis(hashtag_name):
