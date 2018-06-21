0aa2231566121a2ff8240a05d44c232bd30754ab
Maxime Poulain <poulain@et.esiea.fr>
2018-06-21 11:58:54 +0200

    FRAPER preparation

--- DjangoWorldCup/worldcup/views.py
+++ DjangoWorldCup/worldcup/views.py
@@ -5,7 +5,7 @@ import os
 # django project name is adleads, replace adleads with your project name
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "adleads.settings")
 from .tools import countriesCalculations, playersCalculations, players_postCalculations, getPays, getTables, barplot_positivity, positivity_negativity, get_Emojis, getEmojisClassement
-MATCH_HASHTAG = '#URUKSA'
+MATCH_HASHTAG = '#FRAPER'
 
 def home(request):
     if (request.method == "POST"):
@@ -44,12 +44,11 @@ def data_playersPosition(request):
 
 
 def main(request):
-    date = "Wenedsay, June 20th - 17:00"
+    date = "Thursday, June 21th - 17:00"
     country_1, country_2 = getPays(MATCH_HASHTAG)
     emojis = get_Emojis(MATCH_HASHTAG)
-    print(emojis)
-    our_tweet = ""
-    goal_tweet = ""
+    our_tweet = "<blockquote class='twitter-tweet' data-lang='en'><p lang='fr' dir='ltr'>Répondre présent face à une équipe du Pérou qui ne lâchera rien pendant 90 minutes ! On le sait et on est prêt 👊🔥 <a href='https://twitter.com/hashtag/FiersdetreBleus?src=hash&amp;ref_src=twsrc%5Etfw'>#FiersdetreBleus</a><br>🇫🇷🆚🇵🇪<br>🕔 17h<br>📺 TF1 &amp; beIN SPORTS<br>📍 Ekaterinbourg<br>📲 <a href='https://twitter.com/hashtag/FRAPER?src=hash&amp;ref_src=twsrc%5Etfw'>#FRAPER</a> <a href='https://t.co/EfqxtYc1rV'>pic.twitter.com/EfqxtYc1rV</a></p>&mdash; Equipe de France (@equipedefrance) <a href='https://twitter.com/equipedefrance/status/1009685849917853696?ref_src=twsrc%5Etfw'>June 21, 2018</a></blockquote> <script async src='https://platform.twitter.com/widgets.js' charset='utf-8'></script>"
+    goal_tweet = "<blockquote class='twitter-tweet' data-lang='en'><p lang='fr' dir='ltr'>Ce matin, N&#39;Golo Kanté a repris deux fois des Chocapic, Olivier Giroud a enfilé son bandana de ninja, et Deschamps a attaché les mains d&#39;Umtiti dans son dos : désolé, Pérou, mais c&#39;est une machine de guerre qui te fonce dessus. <a href='https://twitter.com/hashtag/FRAPER?src=hash&amp;ref_src=twsrc%5Etfw'>#FRAPER</a> <a href='https://t.co/d9Ued4VWRZ'>https://t.co/d9Ued4VWRZ</a></p>&mdash; SO FOOT (@sofoot) <a href='https://twitter.com/sofoot/status/1009688994744070145?ref_src=twsrc%5Etfw'>June 21, 2018</a></blockquote> <script async src='https://platform.twitter.com/widgets.js' charset='utf-8'></script>"
     context = {'hashtag': MATCH_HASHTAG, 'country_1': country_1, "country_2": country_2, "date":date, "our_tweet": our_tweet, "goal_tweet":goal_tweet, "emojis":emojis}
     return render(request, 'worldcup/main.html', context)
 
@@ -170,11 +169,16 @@ def positive_negative(request):
 #     context = {'hashtag': hashtag, 'country_1': country_1, "country_2": country_2}
 #     return render(request, 'worldcup/match.html', context)
 #
-# def irnspa(request):
-#     hashtag = "#IRNSPA"
-#     country_1, country_2 = getPays(hashtag)
-#     context = {'hashtag': hashtag, 'country_1': country_1, "country_2": country_2}
-#     return render(request, 'worldcup/match.html', context)
+def uruksa(request):
+    hashtag = "#URUKSA"
+    our_tweet = ""#"<blockquote class='twitter-tweet' data-lang='fr'><p lang='fr' dir='ltr'>Les mecs qui parient depuis le début de la Coupe du monde : <a href='https://t.co/s42v98D1nv'>pic.twitter.com/s42v98D1nv</a></p>&mdash; Dark (@Nelson_Menfoot) <a href='https://twitter.com/Nelson_Menfoot/status/1009072056284172290?ref_src=twsrc%5Etfw'>19 juin 2018</a></blockquote>"
+    goal_tweet = ""#"<blockquote class='twitter-tweet' data-lang='en'><p lang='en' dir='ltr'>World Cup 2018: Russia vs Egypt 3-1 all goals <a href='https://t.co/JzQ9cWzeqV'>pic.twitter.com/JzQ9cWzeqV</a></p>&mdash; Daily Hurriyat (@HurriyatPk) <a href='https://twitter.com/HurriyatPk/status/1009173576489078787?ref_src=twsrc%5Etfw'>June 19, 2018</a></blockquote>"
+    rnn_tweet = ""#"<blockquote class='twitter-tweet' data-lang='en'><p lang='en' dir='ltr'>Salah is back for Egypt own goal in 28 years. <a href='https://twitter.com/hashtag/RUSEGY?src=hash&amp;ref_src=twsrc%5Etfw'>#RUSEGY</a> <a href='https://twitter.com/hashtag/RNN?src=hash&amp;ref_src=twsrc%5Etfw'>#RNN</a> <a href='https://t.co/r32RIEslwY'>pic.twitter.com/r32RIEslwY</a></p>&mdash; DataDeer (@DataDeer_) <a href='https://twitter.com/DataDeer_/status/1009398332987502592?ref_src=twsrc%5Etfw'>June 20, 2018</a></blockquote>"
+    date = "Wednesday, June 20th - 17:00"
+    country_1, country_2 = getPays(hashtag)
+    emojis = getEmojisClassement(hashtag[1:])
+    context = {'hashtag': hashtag, 'country_1': country_1, "country_2": country_2,"date":date,"our_tweet": our_tweet, "goal_tweet":goal_tweet, "rnn_tweet": rnn_tweet,"emojis":emojis}
+    return render(request, 'worldcup/match.html', context)
 
 
 def polsen(request):
